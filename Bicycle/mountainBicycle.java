b18f0cec2ee6fa8bfdcc5a2001b59a9a851be26c
JudyJin <jinzhao0531@qq.com>
2018-03-08 08:20:44 +0800

    Adding Mountain Bicycle code

---
+++ Bicycle/mountainBicycle.java
@@ -0,0 +1,20 @@
+
+/**
+ * A class for mountain bicycle.
+ * Instructor: Daniel Gunn
+ * Author: Judy Jin
+ * Date: Mar 8, 2018
+ */
+public class mountainBicycle extends bicycle
+{
+    public mountainBicycle()
+    {
+        super();
+        type="Mountain Bike";
+    }
+    public mountainBicycle(String setter)
+    {
+        super(setter);
+        type="Mountain Bike";
+    }
+}
